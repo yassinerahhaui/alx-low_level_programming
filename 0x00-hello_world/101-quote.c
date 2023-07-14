0e27280637aa877d2edcb8652b54cafa26c1a690
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-07-14 11:35:29 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,10 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *message =
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = strlen(message);
+
 	write(STDERR_FILENO, message, len);
 	return (1);
 }
