f357dba60fed15b89fb2a540b99ef31a48991fb0
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-07-13 20:28:05 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
 int main(void)
 {
-	write(2, ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"), 58);
-	return (1);
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    	write(STDERR_FILENO, message, strlen(message));
+	return (0);
 }
