a13b9a0b45e4ed8561e6bd44b483b357ecd48347
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-07-19 19:26:19 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to the standard error
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
