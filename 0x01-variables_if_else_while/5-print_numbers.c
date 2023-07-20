810dc8791f206fdca64bd0ba21b5a3f7b2a1cc24
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-07-19 19:33:47 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to the standard error
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
