8d4062764926be66fa8ffc12146df212b2390810
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-08-11 19:01:44 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _abs - computes the absolute value of n
+  * @n: number to computes
+  * Return: the absolute value
+  */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
