14f85791f2825d13c91431329a905230dbf104e3
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-08-14 17:58:50 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - multiples two numbers
+  * @a: number to multiple
+  * @b: number to multiple
+  * Return: result
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
