126cbe968e45c62a59c6354b25306bc4e51ac954
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-08-17 13:38:49 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description: prints the numbers from 1 to 100
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
