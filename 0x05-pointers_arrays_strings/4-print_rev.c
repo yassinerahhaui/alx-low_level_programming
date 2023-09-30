4ba2a2bcfe68ae25acd286550750573901768217
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-09-30 12:16:22 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string, in reverse
+  * @s: text to print
+  * Return: nothing
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
