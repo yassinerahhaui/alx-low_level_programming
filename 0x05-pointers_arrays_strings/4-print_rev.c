03035cb0662fb859e1f4efc7186d7165d27c9bba
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-09-30 12:48:55 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,19 @@ void print_rev(char *s)
 {
 	int i, j, len;
 
+	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	len = i - 1;
 
-	while (len >= 0)
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[len]);
-		len--;
+		_putchar(s[j]);
 	}
+
 	_putchar('\n');
 }
