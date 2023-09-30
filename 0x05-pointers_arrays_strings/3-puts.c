708b1fbafecde73200c4491d342101c6cdab09a9
yassinerahhaui <yassinerahhaoui12@gmail.com>
2023-09-30 12:03:54 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _puts -  prints a string, followed by a new line
+  * @*str: string to puts
+  * Return: nothing
+  */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
